138c0bab5e7d9327d2a774a4f7df312ec1352011
Zohaib Shah <bikakparvez@gmail.com>
2026-01-13 11:41:20 +0500

    add

--- src/main.py
+++ src/main.py
@@ -118,7 +118,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
+    allow_origins=settings.cors_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
